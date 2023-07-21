b6d1aea7edf909c86e44797c500a585eee4e2323
bavshehata <bavshehata@gmail.com>
2023-07-21 21:24:27 +0300

    Using models in code and date formatting

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 # Create your views here.
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'July 12, 2020'
-    },
-    {
-        'author': 'BavlyS',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'July 25, 2020'
-    },
-]
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
     }
     return render(request, 'blog/home.html', context)
 
