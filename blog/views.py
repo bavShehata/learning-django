33f48224804f0c5f63bdbaecfa37c348b3384190
bavshehata <bavshehata@gmail.com>
2023-07-21 19:21:40 +0300

    Template context

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 # Create your views here.
 
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'July 12, 2020'
+    },
+    {
+        'author': 'BavlyS',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'July 25, 2020'
+    },
+]
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
